3ce07754ac64025a086369eaddb1b3ff9fc998ed
Himanshu Naidu <hnaidu36@uw.edu>
2024-11-24 08:42:06 +0000

    Remove redundant logic from index entities

--- yago/db/index_entities.py
+++ yago/db/index_entities.py
@@ -10,12 +10,6 @@ from .classes import Item, Property, Claim
 from .constants.main import DB_NAME
 from .yagodb import YagoDB
 
-TTL_PATH = os.path.join(os.path.dirname(__file__), 'data/yago-facts-sample.ttl')
-
-PREFIX_PATH = os.path.join(os.path.dirname(__file__), 'yago-prefixes.txt')
-ERROR_PATH = os.path.join(os.path.dirname(__file__), 'error.txt')
-
-error_file = None # open(ERROR_PATH, 'a')
 
 def create_index_on_items_table(connection, cursor, item_column: str):
     query = f'CREATE INDEX {item_column}_index ON items({item_column});'
@@ -34,6 +28,4 @@ if __name__=="__main__":
     parser.add_argument('--db_name', type=str, default=DB_NAME, help='Name of the database.')
     args = parser.parse_args()
     
-    error_file = open(ERROR_PATH, 'a')
     main(args.db_name)
-    error_file.close()
